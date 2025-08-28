1eec5740cbb12d03a6f7071b3d44fe7b9ba0e9a8
phihungvo <vophihung987@gmail.com>
2025-08-28 10:06:16 +0700

    add MinIO integration for file storage; update docker-compose, configs, and dependencies; provide .env.example files

---
+++ tevc_cms_api/src/main/java/carevn/luv2code/cms/tevc_cms_api/configuration/MinioService.java
@@ -0,0 +1,46 @@
+package carevn.luv2code.cms.tevc_cms_api.configuration;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.annotation.PostConstruct;
+
+@Service
+public class MinioService {
+
+    @Autowired
+    private MinioClient minioClient;
+
+    @Value("${minio.bucket}")
+    private String bucket;
+
+    @PostConstruct
+    public void init() {
+        try {
+            boolean found = minioClient.bucketExists(
+                    BucketExistsArgs.builder().bucket(bucket).build());
+            if (!found) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucket).build());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Error initializing MinIO bucket", e);
+        }
+    }
+
+    public String uploadFile(MultipartFile file) {
+        try {
+            String fileName = UUID.randomUUID() + "-" + file.getOriginalFilename();
+            minioClient.putObject(PutObjectArgs.builder().bucket(bucket).object(fileName).stream(
+                            file.getInputStream(), file.getSize(), -1)
+                    .contentType(file.getContentType())
+                    .build());
+            return fileName;
+        } catch (Exception e) {
+            throw new RuntimeException("Error uploading file to MinIO", e);
+        }
+    }
+}
